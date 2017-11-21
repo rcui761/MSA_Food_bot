38cb844b47a772f9c73a4f3caa95d80a2793afeb
rcui761 <ambercui199528@gmail.com>
2017-11-22 12:45:23 +1300

    foof

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var lastSaid = "";
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+    //session.send("You said: %s", session.message.text);
 	if (lastSaid == session.message.text){
 		session.send("You said that already.");
 		
