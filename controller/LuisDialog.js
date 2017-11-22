032dd9c92177c44eeabc06d0adc7a83e76a5c4da
rcui761 <ambercui199528@gmail.com>
2017-11-22 15:01:41 +1300

    mm

--- controller/LuisDialog.js
+++ controller/LuisDialog.js
@@ -4,7 +4,7 @@ var builder = require('botbuilder');
 exports.startDialog = function (bot) {
     
     // Replace {YOUR_APP_ID_HERE} and {YOUR_KEY_HERE} with your LUIS app ID and your LUIS key, respectively.
-    var recognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/a80fea42-4112-4042-8cb2-c1831bd02c7c?subscription-key=2707128c13c84e70926996fd888d8d4e&verbose=true&timezoneOffset=0&q=	');
+    var recognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/a80fea42-4112-4042-8cb2-c1831bd02c7c?subscription-key=2707128c13c84e70926996fd888d8d4e&verbose=true&timezoneOffset=0&q=');
 
     bot.recognizer(recognizer);
 	bot.dialog('GetCalories', function(session, args){
